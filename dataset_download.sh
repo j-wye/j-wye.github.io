0723b4e200613974ac7843320d9d4f5e991988e6
j-wye <4mimprove@gmail.com>
2024-04-11 12:25:28 +0900

    0411

---
+++ dataset_download.sh
@@ -0,0 +1,7 @@
+# asd
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_color.zip
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_velodyne.zip
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_calib.zip
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip
+axel https://s3.eu-central-1.amazonaws.com/avg-kitti/devkit_odometry.zip
