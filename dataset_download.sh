ffa655bdc883b2782364c8e2948aefbd92b15138
j-wye <114757184+j-wye@users.noreply.github.com>
2024-04-11 12:28:49 +0900

    0411

--- dataset_download.sh
+++ dataset_download.sh
@@ -1,4 +1,5 @@
 # https://docs.google.com/uc?export=download&id=1pYPJpHcAWYk2xKw6qsXFS_hn0kS3bV9x&confirm=t
+# https://s3.eu-central-1.amazonaws.com/avg-kitti 이 주소에서 뒤에 key값을 찾아서 붙여넣으면 다운로드 가능
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_color.zip
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_velodyne.zip
