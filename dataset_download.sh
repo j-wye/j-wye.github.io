10a48684d90af9b8ae087db68fba38f4de24c508
j-wye <4mimprove@gmail.com>
2024-04-11 12:25:52 +0900

    0411

--- dataset_download.sh
+++ dataset_download.sh
@@ -1,4 +1,4 @@
-# asd
+# https://docs.google.com/uc?export=download&id=1pYPJpHcAWYk2xKw6qsXFS_hn0kS3bV9x&confirm=t
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_color.zip
 axel https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_velodyne.zip
