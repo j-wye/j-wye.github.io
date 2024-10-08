8fa0217985258551fb4573de5b0cfe9109028960
j-wye <4mimprove@gmail.com>
2024-10-08 20:20:43 +0900

    1008

---
+++ isaac_base_setup.sh
@@ -0,0 +1,3 @@
+pip install --upgrade pip
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu118
+pip install stable-baselines3==2.0.0 tensorboard==2.11.0 tensorboard-plugin-wit==1.8.1 protobuf==3.20.3
